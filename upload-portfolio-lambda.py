ab356bbd3723f6b68d231b685521e67dc56658ff
Nicholas Proudfoot <nicholas.proudfoot@gmail.com>
2017-10-18 15:07:26 +1100

    refactored python script import statements

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,17 +1,18 @@
 import boto3
+import StringIO
+import zipfile
 
 from botocore.client import Config
+
 s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
 portfolio_bucket = s3.Bucket('portfolio.alchemy-red.com')
 build_bucket = s3.Bucket('portfolio.alchemy-red.com-build')
 
 
-import StringIO
 portfolio_zip = StringIO.StringIO()
 build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-import zipfile
 with zipfile.ZipFile(portfolio_zip) as myzip:
     for nm in myzip.namelist():
         obj = myzip.open(nm)
