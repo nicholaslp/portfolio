7ba9ebea0b9633d3d04a73b80ca4d6eb55be6c4c
Nicholas Proudfoot <nicholas.proudfoot@gmail.com>
2017-10-20 11:42:34 +1100

    added react files

---
+++ js/main.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
+
+const myWork = [
+  {
+    'title': "Work Example 1",
+    'image': {
+      'desc': "Work Example 1",
+      'src': "images/example1.png",
+      'comment': "Work Example 1"
+    }
+  },
+  {
+    'title': "Work Example 2",
+    'image': {
+      'desc': "Serverless",
+      'src': "images/example2.png",
+      'comment': "A Serverless Portfolio"
+    }
+  },
+  {
+    'title': "Work Example",
+    'image': {
+      'desc': "Work Example 3",
+      'src': "images/example3.png",
+      'comment': `“Bengal cat” by roberto shabs is licensed under CC BY 2.0
+             https://www.flickr.com/photos/37287295@N00/2540855181`
+    }
+  }
+]
+
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
